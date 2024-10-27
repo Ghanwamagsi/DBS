635d2228f5218013a1cd38efb6e6560fe0b6aefc
Ghanwa <151755894+Ghanwamagsi@users.noreply.github.com>
2024-10-27 16:54:38 +0500

    Add files via upload

---
+++ company.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employee (
+	"emp_id"	VARCHAR(20),
+	"NAME"	CHAR(20) NOT NULL,
+	"Age"	INTEGER NOT NULL,
+	"R_Address"	VARCHAR(25) NOT NULL,
+	"Salary"	float NOT NULL,
+	PRIMARY KEY("emp_id")
+);
