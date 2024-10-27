3107b6d564e1707a109890f195d1bef2adea941a
Ghanwa <151755894+Ghanwamagsi@users.noreply.github.com>
2024-10-27 16:56:06 +0500

    Add files via upload

---
+++ database system.sql
@@ -0,0 +1,17 @@
+INSERT INTO 
+course(c_info,c_name,c_teacher,creadit_hours)
+VALUES
+("2","DATABASE","SIR OWAIS",3),
+("3","DS","SIR OWAIS",3),
+("2","INTRODUCATION TO AI","SIR AKHTR JALBANI",5),
+("2","STATISTICS","MAAM ISHA TAHIR",5);
+
+SELECT*FROM Courses;
+
+INSERT INTO "st_info"(st_info","adm_date","st_name","st_age","st_address")
+VALUES
+('1',2023-10-26','kausar'19,nawabshsh),
+('2',2023-10-26','Ghanwa'17,nawabshsh),
+('3',2023-10-26','Aqsa'17,karachi),
+('4',2023-10-26','seema'18,'Hyderabad' ),
+
