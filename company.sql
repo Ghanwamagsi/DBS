9f6f36c9ed4c4370c09380342d84c1e34fa9695e
Ghanwa <151755894+Ghanwamagsi@users.noreply.github.com>
2024-10-27 16:59:38 +0500

    Delete company.sql

--- company.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Employee (
-	"emp_id"	VARCHAR(20),
-	"NAME"	CHAR(20) NOT NULL,
-	"Age"	INTEGER NOT NULL,
-	"R_Address"	VARCHAR(25) NOT NULL,
-	"Salary"	float NOT NULL,
-	PRIMARY KEY("emp_id")
-);
